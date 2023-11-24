4df82f7cceea75afee26c549774249232738158d
Ian Kersz <kerszamaral@gmail.com>
2023-11-24 14:45:18 -0300

    Bitstream works

--- example_tb/core/tb_axi.sv
+++ example_tb/core/tb_axi.sv
@@ -26,8 +26,8 @@ module tb_axi #(
   // comment to record execution trace
   //`define TRACE_EXECUTION
 
-  const time CLK_PHASE_HI = 20ns;
-  const time CLK_PHASE_LO = 20ns;
+  const time CLK_PHASE_HI = 5ns;
+  const time CLK_PHASE_LO = 5ns;
   const time CLK_PERIOD = CLK_PHASE_HI + CLK_PHASE_LO;
 
   const time STIM_APPLICATION_DEL = CLK_PERIOD * 0.1;
@@ -111,9 +111,25 @@ module tb_axi #(
 
   logic [7:0] rxData;
   logic rxValid;
+  
+      logic clk_s;
+    reg[27:0] counter=28'd0;
+localparam DIVISOR = 28'd4;
+// The frequency of the output clk_out
+//  = The frequency of the input clk_in divided by DIVISOR
+// For example: Fclk_in = 50Mhz, if you want to get 1Hz signal to blink LEDs
+// You will modify the DIVISOR parameter value to 28'd50.000.000
+// Then the frequency of the output clk_out = 50Mhz/50.000.000 = 1Hz
+always @(posedge clk)
+begin
+ counter <= counter + 28'd1;
+ if(counter>=(DIVISOR-1))
+  counter <= 28'd0;
+ clk_s <= (counter<DIVISOR/2)?1'b1:1'b0;
+end
 
   axi_uartlite_0 uart (
-      .s_axi_aclk   (clk),     // input wire s_axi_aclk
+      .s_axi_aclk   (clk_s),     // input wire s_axi_aclk
       .s_axi_aresetn(rst_n),  // input wire s_axi_aresetn
 
       .interrupt(),  // output wire interrupt
@@ -142,7 +158,7 @@ module tb_axi #(
   );
 
   // print to stdout pseudo peripheral
-  always_ff @(posedge clk, negedge rst_n) begin : print_peripheral
+  always_ff @(posedge clk_s, negedge rst_n) begin : print_peripheral
     if (rxValid && rxData) begin
       $write("%c", rxData);
 
--- rtl/axi/axi_subsystem.sv
+++ rtl/axi/axi_subsystem.sv
@@ -28,6 +28,22 @@ module axi_subsystem #(
     output logic tx_o
 );
 
+    logic clk;
+    reg[27:0] counter=28'd0;
+localparam DIVISOR = 28'd4;
+// The frequency of the output clk_out
+//  = The frequency of the input clk_in divided by DIVISOR
+// For example: Fclk_in = 50Mhz, if you want to get 1Hz signal to blink LEDs
+// You will modify the DIVISOR parameter value to 28'd50.000.000
+// Then the frequency of the output clk_out = 50Mhz/50.000.000 = 1Hz
+always @(posedge clk_i)
+begin
+ counter <= counter + 28'd1;
+ if(counter>=(DIVISOR-1))
+  counter <= 28'd0;
+ clk <= (counter<DIVISOR/2)?1'b1:1'b0;
+end
+
   logic [31:0] exit_value;
 
   always_comb begin
@@ -184,7 +200,7 @@ module axi_subsystem #(
       .REGISTERED_GRANT  (REGISTERED_GRANT)
   ) top_i (
       // Clock and Reset
-      .clk_i          (clk_i),
+      .clk_i          (clk),
       .rst_ni         (rst_ni),
       .pulp_clock_en_i(1'b1),
       // PULP clock enable (only used if COREV_CLUSTER = 1)
@@ -344,7 +360,7 @@ module axi_subsystem #(
       .AXI4_USER_WIDTH   (AXI4_USER_WIDTH),
       .LOGGING           (LOGGING)
   ) u_axi_mm_ram (
-      .clk_i            (clk_i),
+      .clk_i            (clk),
       .rst_ni           (rst_ni),
       //! AXI4 Instruction Interface
       //AXI write address bus -------------- // USED// -----------
